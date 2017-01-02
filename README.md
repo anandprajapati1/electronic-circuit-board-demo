7448479b52ba04701d777aed650e5b44ed5e783f
anandprajapati1 <apraj2@sapient.com>
2017-01-02 16:16:51 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # electronic-circuit-board-demo
 This example emulates a energy consumption in a simple circuit board having a cell as a power source, some equipment and also power supply.
+
+## Getting Started
+### Installing packages
+Clone repository and run following command in command prompt/terminal.
+```
+npm install
+```
+
+### Run in browser
+Run following command in command prompt/terminal
+```
+npm start
+```
+ENJOY!!!
---
+++ ec.js
@@ -0,0 +1,87 @@
+var app = angular.module('electronicCircuitApp', [])
+
+app.controller('ECController', function ($scope) {
+  // >> Battery module
+  $scope.Battery = {
+    charge: 3000,
+    chargePercent: function () {return Math.floor(this.charge * 100 / this.capacity)},
+    isCharging: false,
+    dischargeRate: 0, // neglect auto discharge for now
+    intId: 0,
+    PlugIn: function () {
+      var self = this
+      this.intId = setInterval(function () {
+        self.isCharging = true
+        if ((self.charge / self.capacity) >= 1) {
+          // clearInterval(self.intId)
+          self.timeRemainingToFull = 0
+          $scope.$apply()
+          return
+        }
+        if ((self.capacity - self.charge) < 20)
+          self.charge = self.capacity
+        else
+          self.charge += 20
+        self.timeRemainingToFull = (self.capacity - self.charge) / 10
+        $scope.$apply()
+      }, 100)
+    },
+    PlugOut: function () {
+      this.isCharging = false
+      clearInterval(this.intId)
+    },
+    discharge: function () {
+      var self = this
+      setInterval(function () {
+        if(self.isCharging==false)
+          self.charge -= self.dischargeRate
+        $scope.$apply()
+      }, 100)
+    },
+    timeRemainingToFull: 0
+  }
+  Object.defineProperty($scope.Battery, 'capacity', {writable: false,value: 4000})
+  //   Object.defineProperty($scope.Battery, 'intId', {value: 0})
+
+  $scope.chargeSwitchClick = function () {
+    if ($scope.Battery.isCharging)
+      $scope.Battery.PlugOut()
+    else
+      $scope.Battery.PlugIn()
+  }
+
+  $scope.equipments = []
+  var bulb1 = new EC('BULB', 1)
+  var bulb2 = new EC('BULB', 2)
+  var fan = new EC('FAN', 5)
+  $scope.equipments.push(bulb1)
+  $scope.equipments.push(bulb2)
+  $scope.equipments.push(fan)
+
+  // Equipment class
+  function EC (t, cr) {
+    var consumptionRate = cr
+    this.type = t
+    this.isOn = false
+    var self = this
+    var switchOn = function () {
+        $scope.Battery.dischargeRate += consumptionRate
+        self.isOn = true
+      },
+      switchOff = function () {
+        // prevent negative value for dischargeRate
+        if ($scope.Battery.dischargeRate >= consumptionRate)
+          $scope.Battery.dischargeRate -= consumptionRate
+        self.isOn = false
+    }
+    this.switchToggle = function () {
+      if (self.isOn)
+        switchOff()
+      else
+        switchOn()
+    // $scope.$apply()
+    }
+  }
+
+  $scope.Battery.discharge()
+})
